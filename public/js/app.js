875ca6425cf63153066e55ecb0358def533c2815
Emine Oraklabel <emine.oraklabel@softwire.com>
2025-02-06 15:38:19 +0000

    added change colour button

--- public/js/app.js
+++ public/js/app.js
@@ -6,3 +6,9 @@ function toggleMenu(x) {
   console.log('inside toggle');
   x.classList.toggle("change");
 }
+
+function changeColour(elem) {
+  let postId = document.getElementById(elem);
+  postId.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+ }
+ 
