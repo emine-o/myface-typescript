2bf612bf0355f806d09d33058fb902a563e1b348
Emine Oraklabel <emine.oraklabel@softwire.com>
2025-02-07 12:10:03 +0000

    added menubar

--- public/js/app.js
+++ public/js/app.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add your JavaScript here.
   
 });
-function displayMobileMenu(obj) {
-  console.log('inside toggle');
-  let menuId=document.getElementById('mobileMenu');
- menuId.style.display ='block';
- menuId.classList.add("change");
-}
+
 
 function changeColour(elem) {
   let postId = document.getElementById(elem);
   postId.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
  }
  
+ function displayMobileMenu(obj) {
+  let menuId=document.getElementById('mobileMenu');
+  menuId.style.display ==='block' ? menuId.style.display ='none' :menuId.style.display ='block';
+  obj.classList.toggle("change");
+}
